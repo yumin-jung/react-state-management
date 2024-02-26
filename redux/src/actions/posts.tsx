b7ed8025aa8bc1ca36ebee2aa6586a26d129bee4
yumin-jung <jungym887@naver.com>
2024-02-26 18:09:21 +0900

    redux

---
+++ redux/src/actions/posts.tsx
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const fetchPosts = (): any => async function fetchPostsThunk(dispatch: any, getState: any) {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+    dispatch({ type: "FETCH_POSTS", payload: response.data })
+}
